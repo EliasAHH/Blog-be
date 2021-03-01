3d4262b775b2f7cca66f757a452ecc337c143885
EliasAHH <eliasiscoding@gmail.com>
2021-03-01 15:48:50 -0800

    lecture done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
     def index
         # render json: post.to_json(except: [:created_at, :updated_at,:author_id], include: {author: {only: [:name]}})
+        post = Post.all 
+        # render json: post
         render json: Post.arr_to_json
         #json behind the scenes turns your hash into a string
         #http can only send/receive/read information in STRING format
@@ -10,6 +12,7 @@ class PostsController < ApplicationController
         #}"
     end 
 
+    #"/posts/:id"
     def show
         post = Post.find(params[:id])
         render json: post.instance_to_json 
@@ -17,6 +20,18 @@ class PostsController < ApplicationController
 
     def create
         post = Post.new(post_params)
+        author = Author.find_or_create_by(name: params[:author])
+        post.author_id =  author.id
+        if post.save
+            render json: post.instance_to_json
+        else 
+            render json: post.errors, status: :unprocessable_entity
+        end
+    end 
+
+    def update
+        post = Post.find_by(id:params[:id])
+        post.update(likes:params[:likes])
         if post.save
             render json: post.instance_to_json
         else 
